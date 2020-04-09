38d7f63bc05c026d8db478f23eb2ea4aace037b2
Alex Wachira <4lexchi@live.com>
2020-04-09 19:44:57 +0300

    added comments

--- src/estimator.py
+++ src/estimator.py
@@ -1,29 +1,49 @@
-data = {
+# The data provided
+
+"""data = {
   "region": {
     "name": "Africa",
     "avgAge": 19.7,
     "avgDailyIncomeInUSD": 5,
     "avgDailyIncomePopulation": 0.71
   },
-  "periodType": "days",
-  "timeToElapse": 58,
+  "periodType": "weeks",
+  "timeToElapse": 8,
   "reportedCases": 674,
   "population": 66622705,
   "totalHospitalBeds": 1380614
 }
+"""
 
-def estimator(data):
-  days = 0
+
+def estimator(data): # this function outputs a dictionary after working on the data provided
+  duration = data["timeToElapse"]
+  if data["periodType"] == "days": # if the duration provided in the data is in days
+    duration *= 1
+  if data["periodType"] == "weeks": # if the duration provided in the data is in weeks
+    duration *= 7
+  if data["periodType"] == "months": # if the duration provided in the data is in weeks
+    duration *= 30
+
   currentlyInfected = data["reportedCases"] * 10
   severeImpact = data["reportedCases"] * 50
+
   data = {
-  "data": data,
-  "impact": currentlyInfected,
-  "severeImpact": severeImpact
+  "data": data, # the data provided
+  "impact": {"currentlyInfected": currentlyInfected, "infectionsByRequestedTime": currentlyInfected},
+  "severeImpact": {"currentlyInfected": severeImpact, "infectionsByRequestedTime": severeImpact}
   }
-  data["impact"] = data["impact"] * (2 ** (days//3))
-  data["severeImpact"] = data["severeImpact"] * (2 ** (days//3))
- # print(data)
-  return(data)
+
+  x = data["impact"] # creates a dictionary from data
+  y = data["severeImpact"] # creates a dictionary from data
+
+  x["infectionsByRequestedTime"] = x["infectionsByRequestedTime"] * (2 ** (duration // 3)) # calculates the projected number of infections
+  y["infectionsByRequestedTime"] = y["infectionsByRequestedTime"] * (2 ** (duration // 3)) # calculates the projected number of severe infections
+
+# print(type(data))
+# print(data)
+
+  return(data) # outputs the worked on data
+ 
 
 # estimator(data)
