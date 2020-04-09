ed85d5513345c02082f87cab62e7cfc1786e0b0f
Alex Wachira <4lexchi@live.com>
2020-04-09 12:33:38 +0300

    update estimator

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,31 @@
-def estimator(data):
-  return data
+data = {
+  "region": {
+    "name": "Africa",
+    "avgAge": 19.7,
+    "avgDailyIncomeInUSD": 5,
+    "avgDailyIncomePopulation": 0.71
+  },
+  "periodType": "days",
+  "timeToElapse": 58,
+  "reportedCases": 674,
+  "population": 66622705,
+  "totalHospitalBeds": 1380614
+}
+
+currentlyInfected = data["reportedCases"] * 10
+severeImpact = data["reportedCases"] * 50
+output = {
+  "data": data,
+  "impact": currentlyInfected,
+  "severeImpact": severeImpact
+}
+
+days = 0
+
+def estimator(days):
+  ci = output["impact"] * (2 ** (days//3))
+  si = output["severeImpact"] * (2 ** (days//3))
+  # print("impact: " + str(ci) + ", severeImpact: " + str(si))
+  return ci, si
+
+# estimator(30)
