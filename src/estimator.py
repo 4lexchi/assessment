f2d168428cf16fa8ed69000b37c161cb9feb6ef9
Alex Wachira <4lexchi@live.com>
2020-04-10 14:21:32 +0300

    added requested time

--- src/estimator.py
+++ src/estimator.py
@@ -7,8 +7,8 @@
     "avgDailyIncomeInUSD": 5,
     "avgDailyIncomePopulation": 0.71
   },
-  "periodType": "weeks",
-  "timeToElapse": 8,
+  "periodType": "days",
+  "timeToElapse": 58,
   "reportedCases": 674,
   "population": 66622705,
   "totalHospitalBeds": 1380614
@@ -17,46 +17,51 @@
 
 
 def estimator(data): # this function outputs a dictionary after working on the data provided
-  duration = data["timeToElapse"]
-  if data["periodType"] == "days": # if the duration provided in the data is in days
-    duration *= 1
-  if data["periodType"] == "weeks": # if the duration provided in the data is in weeks
-    duration *= 7
-  if data["periodType"] == "months": # if the duration provided in the data is in months
-    duration *= 30
-
+  duration = data["timeToElapse"] # number of days, weeks or months
+  income = data["region"]["avgDailyIncomeInUSD"] # daily income
+  average = data["region"]["avgDailyIncomePopulation"] # percentage that earns the income above
   currentlyInfected = data["reportedCases"] * 10
   severeImpact = data["reportedCases"] * 50
-  severeCasesByRequestedtime = 1
-  hospitalBedsByRequestedTime = 1
   availableHospitalBeds = (data["totalHospitalBeds"] * 0.35)
+  
+
+  if data["periodType"] == "days": # (if the duration provided in the data is in) days
+    duration *= 1
+  if data["periodType"] == "weeks": # "" weeks
+    duration *= 7
+  if data["periodType"] == "months": # "" months
+    duration *= 30
 
   data = {
   "data": data, # the data provided
   "impact": {
     "currentlyInfected": currentlyInfected,
     "infectionsByRequestedTime": currentlyInfected,
-    "severeCasesByRequestedtime": severeCasesByRequestedtime,
-    "hospitalBedsByRequestedTime": hospitalBedsByRequestedTime
     },
 
   "severeImpact": {
     "currentlyInfected": severeImpact,
     "infectionsByRequestedTime": severeImpact,
-    "severeCasesByRequestedtime": severeCasesByRequestedtime,
-    "hospitalBedsByRequestedTime": hospitalBedsByRequestedTime
-    }
+   }
   }
 
   x = data["impact"] # creates a dictionary from data
   y = data["severeImpact"] # creates a dictionary from data
 
-  x["infectionsByRequestedTime"] = x["infectionsByRequestedTime"] * (2 ** (duration // 3)) # (calculates the projected number) of infections
-  y["infectionsByRequestedTime"] = y["infectionsByRequestedTime"] * (2 ** (duration // 3)) # (calculates the projected number) of severe infections
-  x["severeCasesByRequestedtime"] = int(x["infectionsByRequestedTime"] * 0.15) # "" severe cases
-  y["severeCasesByRequestedtime"] = int(y["infectionsByRequestedTime"] * 0.15) # "" severe cases
-  x["hospitalBedsByRequestedTime"] = int(availableHospitalBeds - x["severeCasesByRequestedtime"]) # "" bed availability
-  y["hospitalBedsByRequestedTime"] = int(availableHospitalBeds - y["severeCasesByRequestedtime"]) # "" bed availability
+  x["infectionsByRequestedTime"] = x["infectionsByRequestedTime"] * (2 ** (duration // 3)) # (estimates the projected number of) infections
+  y["infectionsByRequestedTime"] = y["infectionsByRequestedTime"] * (2 ** (duration // 3)) # (estimates the projected number of) severe infections
+  x["severeCasesByRequestedTime"] = int(x["infectionsByRequestedTime"] * 0.15) # "" severe cases
+  y["severeCasesByRequestedTime"] = int(y["infectionsByRequestedTime"] * 0.15) # "" 
+  x["hospitalBedsByRequestedTime"] = int(availableHospitalBeds - x["severeCasesByRequestedTime"]) # "" bed availability
+  y["hospitalBedsByRequestedTime"] = int(availableHospitalBeds - y["severeCasesByRequestedTime"]) # "" 
+  x["casesForICUByRequestedTime"] = int(x["infectionsByRequestedTime"] * 0.05) # "" ICU care
+  y["casesForICUByRequestedTime"] = int(y["infectionsByRequestedTime"] * 0.05) # ""
+  x["casesForVentilatorsByRequestedTime"] = int(x["infectionsByRequestedTime"] * 0.02) # "" cases to require ventilators
+  y["casesForVentilatorsByRequestedTime"] = int(y["infectionsByRequestedTime"] * 0.02) # ""
+  x["dollarsInFlight"] = int(((x["infectionsByRequestedTime"]) * average * income) / duration) # estimates the amount of money lost within the period 
+  y["dollarsInFlight"] = int(((y["infectionsByRequestedTime"]) * average * income) / duration) # ""
+  x.update()
+  y.update()
 
   # print(type(data))
   # print(data)
