e3d506efa6ab6ada02b8cec5ab5181cefaa5f2bf
Alex Wachira <4lexchi@live.com>
2020-04-10 10:29:52 +0300

    added hospital bed availability

--- src/estimator.py
+++ src/estimator.py
@@ -22,26 +22,44 @@ def estimator(data): # this function outputs a dictionary after working on the d
     duration *= 1
   if data["periodType"] == "weeks": # if the duration provided in the data is in weeks
     duration *= 7
-  if data["periodType"] == "months": # if the duration provided in the data is in weeks
+  if data["periodType"] == "months": # if the duration provided in the data is in months
     duration *= 30
 
   currentlyInfected = data["reportedCases"] * 10
   severeImpact = data["reportedCases"] * 50
+  severeCasesByRequestedtime = 1
+  hospitalBedsByRequestedTime = 1
+  availableHospitalBeds = (data["totalHospitalBeds"] * 0.35)
 
   data = {
   "data": data, # the data provided
-  "impact": {"currentlyInfected": currentlyInfected, "infectionsByRequestedTime": currentlyInfected},
-  "severeImpact": {"currentlyInfected": severeImpact, "infectionsByRequestedTime": severeImpact}
+  "impact": {
+    "currentlyInfected": currentlyInfected,
+    "infectionsByRequestedTime": currentlyInfected,
+    "severeCasesByRequestedtime": severeCasesByRequestedtime,
+    "hospitalBedsByRequestedTime": hospitalBedsByRequestedTime
+    },
+
+  "severeImpact": {
+    "currentlyInfected": severeImpact,
+    "infectionsByRequestedTime": severeImpact,
+    "severeCasesByRequestedtime": severeCasesByRequestedtime,
+    "hospitalBedsByRequestedTime": hospitalBedsByRequestedTime
+    }
   }
 
   x = data["impact"] # creates a dictionary from data
   y = data["severeImpact"] # creates a dictionary from data
 
-  x["infectionsByRequestedTime"] = x["infectionsByRequestedTime"] * (2 ** (duration // 3)) # calculates the projected number of infections
-  y["infectionsByRequestedTime"] = y["infectionsByRequestedTime"] * (2 ** (duration // 3)) # calculates the projected number of severe infections
+  x["infectionsByRequestedTime"] = x["infectionsByRequestedTime"] * (2 ** (duration // 3)) # (calculates the projected number) of infections
+  y["infectionsByRequestedTime"] = y["infectionsByRequestedTime"] * (2 ** (duration // 3)) # (calculates the projected number) of severe infections
+  x["severeCasesByRequestedtime"] = int(x["infectionsByRequestedTime"] * 0.15) # "" severe cases
+  y["severeCasesByRequestedtime"] = int(y["infectionsByRequestedTime"] * 0.15) # "" severe cases
+  x["hospitalBedsByRequestedTime"] = int(availableHospitalBeds - x["severeCasesByRequestedtime"]) # "" bed availability
+  y["hospitalBedsByRequestedTime"] = int(availableHospitalBeds - y["severeCasesByRequestedtime"]) # "" bed availability
 
-# print(type(data))
-# print(data)
+  # print(type(data))
+  # print(data)
 
   return(data) # outputs the worked on data
  
